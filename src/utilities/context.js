02f32bc9cebd35a537c10f6e6c724cbeaca88a85
seanmp3 <seanmahansm@gmail.com>
2023-07-19 13:01:43 -0500

    Added React Context file, data, and a few other minor changes

---
+++ src/utilities/context.js
@@ -0,0 +1,41 @@
+import React, {useState, useContext} from "react"
+import sublinks from "../data"
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true)
+  }
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false)
+  }
+
+  return <AppContext.Provider
+   value={{
+    isSidebarOpen,
+    openSidebar,
+    closeSidebar,
+    isSubmenuOpen,
+    openSubmenu,
+    closeSubmenu
+   }}
+  >
+    {children}
+  </AppContext.Provider>
+}
+
+export const GlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
